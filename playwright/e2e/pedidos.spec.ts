0891d0c3bcdcaf24ccc8c61b9ad580036d0e8b26
brayner <brayner1406@gmail.com>
2026-08-31 09:42:37 -0300

    iniciando a automação do teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+
+
+    await page.goto('http://localhost:5173/');
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await page.getByTestId('search-order-id').click();
+    await page.getByTestId('search-order-id').fill('');
+    await page.getByTestId('search-order-id').click();
+    await page.getByTestId('search-order-id').fill('VLO-M4EGPT');
+    await page.getByTestId('search-order-button').click();
+    await expect(page.getByTestId('order-result-id')).toBeVisible();
+    await page.getByTestId('order-result-status').click();
+
+
+
+})
