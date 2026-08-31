c6a7a7d55be7684c5199f7d45ed104e812b248dd
brayner <brayner1406@gmail.com>
2026-08-31 10:14:44 -0300

    AAA - arrange, act, assert atualizado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -2,18 +2,27 @@ import { test, expect } from '@playwright/test';
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
 
-    //arrange
+    //Arrange
+    
     await page.goto('http://localhost:5173/');
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
- 
     
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+    
+    //Act
+
     await page.getByTestId('search-order-id').fill('VLO-M4EGPT');
     await page.getByTestId('search-order-button').click();
 
+    //Assert
 
     await expect(page.getByTestId('order-result-id')).toBeVisible();
-    await page.getByTestId('order-result-status').click();
+    await expect(page.getByTestId('order-result-id')).toContainText('VLO-M4EGPT')
+
+    await expect(page.getByTestId('order-result-status')).toBeVisible();
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+    
 
 
 
