ba5adc4f3cf768d97e97a8efd68d5e9d2dfe0342
brayner <brayner1406@gmail.com>
2026-09-04 09:15:22 -0300

    adição de mais um cenario de testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -107,4 +107,19 @@ test.describe('Consulta de Pedido', () => {
     await orderLockupPage.validateOrderNotFound()
 
   })
+
+  test('deve exibir mensagem quando o pedido em qualquer formato não é encontrado', async ({ page }) => {
+
+   
+
+    const orderLockupPage = new OrderLockupPage(page)
+    await orderLockupPage.searchOrder('abc123')
+
+
+    await orderLockupPage.validateOrderNotFound()
+
+  })
+
+
+
 })
